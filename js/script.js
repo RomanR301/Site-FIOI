7a92816bf9f27abd96758e1d550120c532be5f7c
MAKSYM <roman.sav15@gmail.com>
2020-11-03 22:42:10 +0200

    final

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var swiperTestimonial = new Swiper('.testimonial-carousel', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        loop: true,
+    var carousel = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        watchOverflow: true,
         navigation: {
-            nextEl: '.testimonial-next',
-            prevEl: '.testimonial-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
         pagination: {
-            el: '.testimonial-pagination',
+            el: '.swiper-pagination',
         },
-        breakpoints: {
-            320: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-            autoHeight: true,
-            },
-            767: {
-              slidesPerView: 2,
-            },
-            992: {
-                slidesPerView: 3,
-            },
-        }
     });
 });
 
